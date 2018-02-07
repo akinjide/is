336b2e8253894f48013746d4e54f9dfb983ba6de
Akinjide Bankole <r@akinjide.me>
2018-02-07 22:23:41 +0100

    test: add test and configure travis

---
+++ test/index.js
@@ -0,0 +1,21 @@
+
+/**
+ * Module dependencies.
+ */
+
+var assert = require('assert');
+var is = require('..');
+var size = require('size');
+
+describe('queries', function() {
+  it('should work', function(done) {
+    var promise = size({
+      bytes: 330307828,
+      decimals: 1
+    });
+
+    var actual = is.promise(promise);
+    var expected = true;
+    assert.equal(actual, expected);
+  });
+});
